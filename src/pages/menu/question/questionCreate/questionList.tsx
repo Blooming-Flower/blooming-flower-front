022152d34a7bfc2c76092507e2c3db4b24001032
최은하 <choieunha@choeeunhaui-MacBookAir.local>
2023-09-11 11:08:38 +0900

    questionList 구현 중

--- src/pages/menu/question/questionCreate/questionList.tsx
+++ src/pages/menu/question/questionCreate/questionList.tsx
@@ -1,18 +1,65 @@
-import { Box, List, ListItem, ListItemText } from "@mui/material";
+import {
+  Box,
+  Grid,
+  IconButton,
+  List,
+  ListItem,
+  ListItemText,
+  ListItemAvatar,
+  Avatar,
+  styled,
+} from "@mui/material";
 import React from "react";
+import DeleteIcon from "@mui/icons-material/Delete";
 
-const questionList = () => {
+//css
+import "@css/questionCreate/questionList.scss";
+
+const Demo = styled("div")(({ theme }) => ({
+  backgroundColor: theme.palette.background.paper,
+}));
+
+const generate = (element: React.ReactElement) => {
+  return [0, 1, 2].map((value) =>
+    React.cloneElement(element, {
+      key: value,
+    })
+  );
+};
+
+const QuestionList = () => {
+  const [dense, setDense] = React.useState(false);
+  const [secondary, setSecondary] = React.useState(false);
+
   return (
-    <div>
-      <Box>
-        <List>
-          <ListItem>
-            <ListItemText primary="수능특강" />
-          </ListItem>
-        </List>
-      </Box>
+    <div className="questionList-item">
+      <Grid item xs={6} md={4}>
+        <Demo>
+          <List dense={dense}>
+            {generate(
+              <ListItem
+                secondaryAction={
+                  <IconButton edge="end" aria-label="delete">
+                    <DeleteIcon />
+                  </IconButton>
+                }
+              >
+                {/* <ListItemAvatar>
+                    <Avatar>
+                      <FolderIcon />
+                    </Avatar>
+                  </ListItemAvatar> */}
+                <ListItemText
+                  primary="Single-line item"
+                  secondary={secondary ? "Secondary text" : null}
+                />
+              </ListItem>
+            )}
+          </List>
+        </Demo>
+      </Grid>
     </div>
   );
 };
 
-export default questionList;
+export default QuestionList;
