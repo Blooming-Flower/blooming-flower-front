f0ba35669ccb2364ec8fa70cdb19900ca58c68a9
alswo1212 <minjagot@naver.com>
2023-09-12 23:43:18 +0900

    시험지관리 레이아웃 1

--- src/pages/menu/question/examManage/examMng.tsx
+++ src/pages/menu/question/examManage/examMng.tsx
@@ -1,8 +1,75 @@
 import Layout from "@components/layouts/layout";
+import CustomButton from "@components/ui/button/custeomButton";
+import { FormControl, TextField, Pagination, Button } from "@mui/material";
 import Typography from "@mui/material/Typography";
+import { DataGrid, GridColDef } from "@mui/x-data-grid";
 import * as React from "react";
 
 const ExamMng = () => {
+  const [searcText, setSearchText] = React.useState("");
+  const [page, setPage] = React.useState(0);
+  const popupRef: any = React.useRef();
+
+  const textFieldOnChange = (event : Event) => {
+    const {
+      target: { value },
+    } = event;
+    setSearchText(value);
+  };
+
+  const deleteExam = () => {
+    popupRef.current.handleOpen();
+  };
+
+  const columns: GridColDef[] = [
+    {
+      field: "title",
+      headerName: "Title",
+      width: 700,
+      align: "center",
+      headerAlign: "center",
+    },
+    {
+      field: "createDate",
+      headerName: "제작일자",
+      width: 150,
+      align: "center",
+      headerAlign: "center",
+    },
+    {
+      field: "downPdf",
+      headerName: "다운로드",
+      width: 150,
+      align: "center",
+      headerAlign: "center",
+    },
+    {
+      field: "actions",
+      headerName: "삭제",
+      width: 150,
+      align: "center",
+      headerAlign: "center",
+      getActions: (params) => [
+        <Button
+          variant="outlined"
+          color="warning"
+          size="medium"
+          onClick={deleteExam}
+        >
+          삭제
+        </Button>,
+      ],
+    },
+  ];
+
+  const rows = [
+    {
+      id: 1,
+      title: "[2023-2 중간]종촌고2",
+      createDate: (new Date()).toISOString().split('T')[0],
+    },
+  ];
+
   return (
     <Layout>
       <div className="mainCont">
@@ -13,6 +80,44 @@ const ExamMng = () => {
         >
           시험지관리
         </Typography>
+        <div style={{ textAlign: "right" }}>
+          <TextField
+            // size="small"
+            label="타이틀"
+            variant="outlined"
+            onChange={textFieldOnChange}
+            style={{ marginRight: "20px", width: "400px" }}
+          >
+            {searcText}
+          </TextField>
+          <CustomButton type="string" label="검색"></CustomButton>
+        </div>
+        <div>
+          <DataGrid
+            rows={rows}
+            columns={columns}
+            checkboxSelection
+            disableRowSelectionOnClick
+            initialState={{
+              pagination: {
+                paginationModel: {
+                  pageSize: 10,
+                },
+              },
+            }}
+            sx={{ fontWeight: "500", fontSize: "15px" }}
+            hideFooterPagination={true}
+          />
+          <Pagination
+            count={parseInt((rows.length / 5).toString()) + 1}
+            onChange={(event, value) => setPage(value - 1)}
+            page={page + 1}
+            showFirstButton
+            showLastButton
+            shape="rounded"
+            sx={{ display: "flex" }}
+          />
+        </div>
       </div>
     </Layout>
   );
