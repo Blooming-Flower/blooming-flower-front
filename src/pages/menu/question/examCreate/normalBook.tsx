39357e2daa2d2c48f58030009d2b852e371055b3
alswo1212 <minjagot@naver.com>
2023-10-19 22:27:59 +0900

    시험지 작업 시작

--- src/pages/menu/question/examCreate/normalBook.tsx
+++ src/pages/menu/question/examCreate/normalBook.tsx
@@ -1,15 +1,11 @@
-import {useEffect, useState} from "react";
+import { useEffect, useState } from "react";
 
-const NormalBook = (props:ExamProps) => {
-    const [rowData, setRowData] = useState(props.rowData)
-    useEffect(()=>{
-        console.log('미리보기데이터'+props)
-    },[rowData])
-    return(
-        <div className='div_paper'>
-            시험지다
-        </div>
-    )
-}
+const NormalBook = (props: ExamProps) => {
+  const [rowData, setRowData] = useState(props.rowData);
+  useEffect(() => {
+    console.log("미리보기데이터", props);
+  }, [rowData]);
+  return <div className="div_paper">시험지다</div>;
+};
 
-export default NormalBook
+export default NormalBook;
